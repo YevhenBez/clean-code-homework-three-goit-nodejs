c01b758f7f82eaa2d3f34d58ad052dedd3099fd2
YevhenBez <byxgalter@bigmir.net>
2024-07-19 17:32:22 +0300

    Вынес отдельно мидлвару для проверки значений при добавлении контакта  и при обновлении (изменении) данных контакта. В этой репе исключительно код Домашки номер 3 по Node.js (GoIt)
    Вынес отдельно мидлвару для проверки значений при добавлении контакта и при обновлении (изменении) данных контакта.  Для этого перенёс импорт    Joi    и    переменную    addSchema     с файла    contactsCtrls.js    в файл    contactsSchemas.js    и сразу экспортнул    данную переменную при помощи       module.exports = {
        addSchema,
    }       В файле    validateBody.js   Импортнул   HttpError     и в переменной     validateBody    написал Мидлвару, которая несёт в себе алгоритм проверки значений, которые хочет внести пользователь. И , как резюме, экспортнул её при помощи      module.exports = validateBody;    В связи с этим в файле   contactsCtrls.js       в функции    addContactCtrls    и     updateContactCtrls      убрал алгоритм проверки значений, которые вводит пользователь, а также вывод ошибки, если она появляется. А также в файле contacts.js     который находится в папке     api   в Роутерах добавления контакта и обновления (изменения) значения контактов добавил проверку значений, вводимых пользователем, при помощи   validateBody(schemas.addSchema)
     И также в данном файле импортнул
      схему проверки значений, вводимых пользователем, при помощи   const schemas = require("../../schemas/contactsSchemas");     и импортнул мидлвару по проверке  значений, вводимых пользователем при помощи       const  validateBody  = require("../../middlewares/validateBody");
    Также поудалял все, уже не нужные, закоментированные куски кода
    Я иду исключительно по комитам домашки Модуля 3 Node.js
    В этой репе исключительно код Домашки номер 3 по Node.js (GoIt)

--- controllers/contactsCtrls.js
+++ controllers/contactsCtrls.js
@@ -1,15 +1,10 @@
-const Joi = require("joi");
 
 const contacts = require("../models/contacts");
 
 const { HttpError } = require("../helpers/HttpError");
 const ctrlWrapper = require("../helpers/ctrlWrapper");
 
-const addSchema = Joi.object({
-  name: Joi.string().required(),
-  email: Joi.string().required(),
-  phone: Joi.string().required(),
-});
+
 
 const listContactsCtrls = async (req, res) => {
     const result = await contacts.listContacts();
@@ -29,10 +24,7 @@ const getContactByIdCtrls = async (req, res) => {
 
 const addContactCtrls = async (req, res) => {
   
-    const { error } = addSchema.validate(req.body);
-    if (error) {
-      throw HttpError(400, `${error.message} : missing required name field`);
-    }
+    
     const result = await contacts.addContact(req.body);
     res.status(201).json(result);
   
@@ -50,10 +42,7 @@ const removeContactCtrls = async (req, res) => {
 
 const updateContactCtrls = async (req, res) => {
   
-    const { error } = addSchema.validate(req.body);
-    if (error) {
-      throw HttpError(400, `${error.message} : missing fields`);
-    }
+    
     const { id } = req.params;
     const result = await contacts.updateContact(id, req.body);
     if (!result) {
