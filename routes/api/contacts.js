b4d11cea1e128386d69bf77d7bcd169c6c641d1e
YevhenBez <byxgalter@bigmir.net>
2024-07-24 13:09:58 +0300

    Реализовал получение всех контактов уже с базы MongoDB Atlas   В этой репе исключительно код Домашки номер 3 по Node.js (GoIt) , но при этом я уже подключаю другую базу MongoDB
    Реализовал получение всех контактов уже с базы      MongoDB Atlas   В связи с тем, что логика значительно поменялась то пришлось закоментировать половину кода, который в дальнейшем будет нужен, но сейчас, если оставить его раскоментированным, то он сыпет ошибки и не запускает программу. Итак в файле   contacts.js     который находится в папке    api   я закоментировал код, по причине описанной выше, который в дальнейшем будет нужен.  Единственное, могут быть изменения в переменной     validateBody    и в     schemas     т.к. валидация уже будет скорее всего с библиотеки       Mongoose     В файле    contacts.js     который находится в папке    models      скорее всего не понадобится весь закоментированный код, т.к. тут уже начинает править   библиотека       Mongoose     со своими примочками.  А именно  я импортнул    Schema    для проверки валидации     model     (модель)     с    библиотеки       Mongoose    Затем в переменной    contactsSchema     я задал саму схему проверки значений (согласно ТЗ) и на основании данной схемы я создал модель при помощи    const ContactMongoose = model("contact", contactsSchema);     и затем экспортнул данную модель при помощи     module.exports = ContactMongoose;     В файле     contactsCtrls.js   из закоментированного кода, по причине описанной выше, может пригодиться только импорт      HttpError    также пригодятся сами Контроллеры    getContactByIdCtrls      addContactCtrls    removeContactCtrls      updateContactCtrls      А также закоментировал экспорт выше указанных (также закоментированных) Контроллеров. Также в файле   contactsCtrls.js    импортнул модель    ContactMongoose    созданную на базе валидации при помощи библиотеки    Mongoose    А в функции    listContactsCtrls     при помощи модели    ContactMongoose     и её метода     find     получил все контакты с базы     MongoDB Atlas
    Я иду исключительно по комитам домашки Модуля 3 Node.js
    В этой репе исключительно код Домашки номер 3 по Node.js (GoIt)  , но при этом я уже подключаю другую базу   MongoDB

--- routes/api/contacts.js
+++ routes/api/contacts.js
@@ -2,9 +2,9 @@ const express = require("express");
 
 const ctrl = require("../../controllers/contactsCtrls");
 
-const validateBody = require("../../middlewares/validateBody");
+// const validateBody = require("../../middlewares/validateBody");
 
-const schemas = require("../../schemas/contactsSchemas");
+// const schemas = require("../../schemas/contactsSchemas");
 
 const router = express.Router();
 
@@ -12,13 +12,13 @@ const router = express.Router();
 
 router.get("/", ctrl.listContactsCtrls);
 
-router.get("/:id", ctrl.getContactByIdCtrls);
+// router.get("/:id", ctrl.getContactByIdCtrls);
 
-router.post("/", validateBody(schemas.addSchema), ctrl.addContactCtrls);
+// router.post("/", validateBody(schemas.addSchema), ctrl.addContactCtrls);
 
 
-router.delete("/:id", ctrl.removeContactCtrls);
+// router.delete("/:id", ctrl.removeContactCtrls);
 
-router.put("/:id", validateBody(schemas.addSchema), ctrl.updateContactCtrls);
+// router.put("/:id", validateBody(schemas.addSchema), ctrl.updateContactCtrls);
 
 module.exports = router;
