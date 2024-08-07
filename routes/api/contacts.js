12b760658018198404b7ebcc5a068af08a9aab82
YevhenBez <byxgalter@bigmir.net>
2024-08-07 22:24:04 +0300

    Все изменения в данных файлах - это я отформатировал при помощи     Prettier   В этой репе исключительно код Домашки номер 3 по Node.js (GoIt)  , но при этом я уже подключаю другую базу   MongoDB
    Все изменения в данных файлах - это я отформатировал при помощи     Prettier
    Я иду исключительно по комитам домашки Модуля 3 Node.js
    В этой репе исключительно код Домашки номер 3 по Node.js (GoIt)  , но при этом я уже подключаю другую базу   MongoDB

--- routes/api/contacts.js
+++ routes/api/contacts.js
@@ -10,19 +10,26 @@ const { schemas } = require("../../models/contacts");
 
 const router = express.Router();
 
-
-
 router.get("/", ctrl.listContactsCtrls);
 
 router.get("/:id", isValidId, ctrl.getContactByIdCtrls);
 
 router.post("/", validateBody(schemas.addSchema), ctrl.addContactCtrls);
 
-
 router.delete("/:id", isValidId, ctrl.removeContactCtrls);
 
-router.put("/:id", isValidId, validateBody(schemas.addSchema), ctrl.updateContactCtrls);
+router.put(
+  "/:id",
+  isValidId,
+  validateBody(schemas.addSchema),
+  ctrl.updateContactCtrls
+);
 
-router.patch("/:id/favorite", isValidId, validateBody(schemas.updateFavoriteSchema), ctrl.updateStatusContactCtrls);
+router.patch(
+  "/:id/favorite",
+  isValidId,
+  validateBody(schemas.updateFavoriteSchema),
+  ctrl.updateStatusContactCtrls
+);
 
 module.exports = router;
